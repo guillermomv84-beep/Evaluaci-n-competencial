db05cdb060303fa1de0c75e239ec58b04b5b6736
guillermomv84-beep <guillermomv84@gmail.com>
2025-09-25 16:38:44 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,38 @@
-fetch("data.json")
-  .then(response => response.json())
-  .then(data => {
-    const app = document.getElementById("app");
-    app.innerHTML = "<pre>" + JSON.stringify(data, null, 2) + "</pre>";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const cursoSelect = document.getElementById("cursoSelect");
+  const areaSelect = document.getElementById("areaSelect");
+  const output = document.getElementById("output");
+
+  fetch('data.json')
+    .then(res => res.json())
+    .then(data => {
+      const cursos = Object.keys(data);
+      cursos.forEach(curso => {
+        const option = document.createElement("option");
+        option.value = curso;
+        option.textContent = curso;
+        cursoSelect.appendChild(option);
+      });
+
+      cursoSelect.addEventListener("change", () => {
+        areaSelect.innerHTML = "";
+        const areas = Object.keys(data[cursoSelect.value]);
+        areas.forEach(area => {
+          const option = document.createElement("option");
+          option.value = area;
+          option.textContent = area;
+          areaSelect.appendChild(option);
+        });
+      });
+
+      areaSelect.addEventListener("change", () => {
+        const selectedData = data[cursoSelect.value][areaSelect.value];
+        output.innerHTML = "<pre>" + JSON.stringify(selectedData, null, 2) + "</pre>";
+      });
+    });
+
+  document.getElementById("exportPdf").addEventListener("click", () => {
+    html2pdf().from(output).save("evaluacion_competencial.pdf");
   });
+});
